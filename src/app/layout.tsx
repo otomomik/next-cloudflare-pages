6be1722ff2328ffbee5520d1e7202b4121e39812
otomomik <developer@otomomik.com>
2023-01-25 22:13:23 +0900

    トップページの修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
 import type { FC, PropsWithChildren } from 'react'
-import './globals.css'
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="ja">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head>
+        <meta name="robots" content="noindex" />
+      </head>
       <body>{children}</body>
     </html>
   )
