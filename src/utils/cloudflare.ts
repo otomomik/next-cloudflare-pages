70d44e8a12d7edb376fdfeca13f6068a1e945f10
otomomik <115955017+otomomik@users.noreply.github.com>
2023-01-26 20:38:46 +0900

    v0.0.2 (#3)

--- src/utils/cloudflare.ts
+++ src/utils/cloudflare.ts
@@ -8,7 +8,7 @@ type Env =
 // eslint-disable-next-line @typescript-eslint/naming-convention
 declare const __ENV__: Env
 
-export const getEnv = (): Env => {
+export const getCloudflareEnv = (): Env => {
   if (typeof window !== 'undefined') {
     return {}
   }
